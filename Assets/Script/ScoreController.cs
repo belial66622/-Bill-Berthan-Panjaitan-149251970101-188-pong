7c5c00cb0cfc48cdddc6a4d000b3d4558e61f683
belial66622 <50286436+belial66622@users.noreply.github.com>
2022-06-09 23:33:17 +0700

    Adding UI & Game Manager
    Adding UI & Game Manager

---
+++ Assets/Script/ScoreController.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Text skorKiri;
+    public Text skorKanan;
+
+    public Scoremanager manager;
+
+    void Update()
+    {
+        skorKiri.text = manager.leftScore.ToString();
+        skorKanan.text = manager.rightScore.ToString();
+
+    }
+}
